0fb7af8d892e3507ec3c0cd8fa6c5ce6a650d997
Carmen <carmen.tang1@gmail.com>
2022-10-04 09:18:31 -0700

    fetch json from axios, displaying first last

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  const getData = () => {
+    axios.get('switchboard.json')
+    .then(res => {
+      // console.log(res);
+      // console.log(res.data);
+      console.log('here');
+      setData(res.data);
+    })
+  }
+
+  useEffect(() => {
+    getData();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {data.slice(0, 10).map(i => {
+          return <div key={i.id}>{i.donor_firstname} {i.donor_lastname}</div>
+        })
+        }
+      </div>
     </div>
   );
 }
